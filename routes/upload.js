64f8052418221838378b6a4d467bf682b7482405
Roshan <its.roshanojha@gmail.com>
2022-12-01 20:58:21 +0530

    Created upload_controller to read csv file and convert it to json and store some information in db

--- routes/upload.js
+++ routes/upload.js
@@ -1,14 +1,15 @@
 /****************IMPORTING PACKAGE*******************************/
-const express = require("express");//it will just fetch the existing instance, will not create new instance
+const express = require("express");
+
 
 /****************USING ROUTER************************************/
 const router = express.Router();
 
 /**************IMPORTING CONTROLLERS*****************************/
-const homeController = require("../controller/home_controller");
+const uploadController = require("../controller/upload_controller");
+/**********************MAKING ROUTES*****************************/
+router.post("/", uploadController.Upload);
 
-router.get("/", homeController.Index);
-router.use("/upload", require("./upload.js"));
 
 /*****************EXPORTING ROUTER*******************************/
 module.exports = router;
